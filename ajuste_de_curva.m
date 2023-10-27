98862651ed5c8e2344002b809742049aa1424f75
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2023-10-27 11:48:53 -0300

    adicao comentarios e tabela para dar load

--- ajuste_de_curva.m
+++ ajuste_de_curva.m
@@ -1,4 +1,7 @@
-x = [1.02 2.04 2.95 3.55]'
-y = [4.55 7.20 9.51 10.81]'
-A = [x ones(4,1)]
-v = inv(A'*A)*A'*y
+% x = [1.02 2.04 2.95 3.55]'
+% y = [4.55 7.20 9.51 10.81]'
+function ajuste_de_curva(x, y)
+    % A = [x ones('numero de linhas','numero de colunas')]
+    A = [x ones(33,1)]
+    v = inv(A'*A)*A'*y
+endfunction
